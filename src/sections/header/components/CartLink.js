761c16ad00796942ad414a4f0e2fa676c51a9224
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-04 14:07:02 +0200

    Fix cart icon display as an inline svg

--- src/sections/header/components/CartLink.js
+++ src/sections/header/components/CartLink.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import cartIcon from '../../../assets/icons/shopping-cart.svg';
+import { ReactComponent as CartIcon } from '../../../assets/icons/shopping-cart.svg';
 
 const CartLink = ({ link, cartCount }) => {
   return (
@@ -16,7 +16,7 @@ const CartLink = ({ link, cartCount }) => {
         ''
       )}
       <a className="cart-link" href={link} aria-label="Cart">
-        {cartIcon}
+        <CartIcon />
       </a>
     </div>
   );
