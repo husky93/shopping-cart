0fd0e8ca11a74ac03d87ea39c19e9e47b5759add
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-04 19:12:08 +0200

    Update Footer component tests

--- src/sections/footer/__tests__/Footer.test.js
+++ src/sections/footer/__tests__/Footer.test.js
@@ -8,4 +8,14 @@ describe('Footer', () => {
     const footer = screen.getByRole('contentinfo');
     expect(footer).toBeInTheDocument();
   });
+  it('has a creator contribution', () => {
+    render(<Footer />);
+    const text = screen.getByText(/created by/i);
+    expect(text).toBeInTheDocument();
+  });
+  it('has 6 links', () => {
+    render(<Footer />);
+    const links = screen.queryAllByRole('link');
+    expect(links.length).toBe(5);
+  });
 });
