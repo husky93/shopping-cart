ae88d2168887975c12d13ebae4d3fe84415b25cb
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-04 16:41:11 +0200

    Create tests for Button component

---
+++ src/components/__tests__/Button.test.js
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import Button from '../Button';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+
+const text = 'Test';
+
+describe('Button', () => {
+  it('renders a button with a specified text', () => {
+    const onClickMock = jest.fn();
+    render(<Button onClick={onClickMock} text={text} className="btn" />);
+    const btn = screen.getByRole('button', { name: /test/i });
+
+    expect(btn).toBeInTheDocument();
+    expect(btn.textContent).toBe('Test');
+  });
+
+  it('calls the provided callback function', () => {
+    const onClickMock = jest.fn();
+    render(<Button onClick={onClickMock} text={text} className="btn" />);
+    const btn = screen.getByRole('button', { name: /test/i });
+
+    userEvent.click(btn);
+    expect(onClickMock).toHaveBeenCalledTimes(1);
+    userEvent.click(btn);
+    expect(onClickMock).toHaveBeenCalledTimes(2);
+  });
+});
