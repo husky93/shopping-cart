1593aea5c9c12263a82dbe1f58919260dee58d60
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-04 18:49:11 +0200

    Update tests for Homepage page component

--- src/pages/homepage/__tests__/Homepage.test.js
+++ src/pages/homepage/__tests__/Homepage.test.js
@@ -1,10 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import Homepage from '../Homepage';
+import '@testing-library/jest-dom';
+
+jest.mock('../components/Hero', () => () => (
+  <section data-testid="hero"></section>
+));
+
+jest.mock('../../../sections/header/Header', () => () => (
+  <header data-testid="header"></header>
+));
 
 describe('Homepage', () => {
   it('renders homepage component', () => {
     render(<Homepage />);
     const main = screen.getByRole('main');
     expect(main).toBeInTheDocument();
+  });
+  it('renders header component inside', () => {
+    render(<Homepage />);
+    const header = screen.getByTestId('header');
+    expect(header).toBeInTheDocument();
+  });
+  it('renders hero component inside', () => {
+    render(<Homepage />);
+    const hero = screen.getByTestId('hero');
+    expect(hero).toBeInTheDocument();
   });
 });
