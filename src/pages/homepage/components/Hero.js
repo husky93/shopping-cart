64e31f9767ab07f6a26e819df8e6302703d450d4
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-04 18:41:19 +0200

    Create Hero component

--- src/pages/homepage/components/Hero.js
+++ src/pages/homepage/components/Hero.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Text from '../../../components/Text';
+import Button from '../../../components/Button';
 
-const Main = () => {
-  return <main className="main"></main>;
+const Hero = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/shop');
+  };
+
+  return (
+    <section className="hero" aria-label="Hero">
+      <div className="hero__content">
+        <Text
+          tag="h2"
+          text={`The Best Bonsai Trees in the World`}
+          className="hero__heading"
+        />
+        <Text
+          tag="p"
+          text={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`}
+          className="hero__subtext"
+        />
+        <Button
+          handleClick={handleClick}
+          text="Find Your Tree"
+          className="btn btn--primary"
+        />
+      </div>
+    </section>
+  );
 };
 
-export default Main;
+export default Hero;
