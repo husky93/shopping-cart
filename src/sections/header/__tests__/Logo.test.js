5c15d9470da3f4a63465c3c6806e66e4030672f7
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-04 13:20:06 +0200

    Create Logo component

--- src/sections/header/__tests__/Logo.test.js
+++ src/sections/header/__tests__/Logo.test.js
@@ -1,6 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import Logo from '../components/Logo';
+import '@testing-library/jest-dom';
 
 describe('Logo', () => {
-  it('is', () => {});
+  it('renders a logo heading with correct text', () => {
+    render(<Logo text="Bonsai" />);
+    const logo = screen.getByRole('heading', 'Bonsai');
+    expect(logo).toBeInTheDocument();
+  });
 });
---
+++ src/sections/header/components/Logo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Logo = ({ text }) => {
+  return <h1 className="logo">{text}</h1>;
+};
+
+export default Logo;
