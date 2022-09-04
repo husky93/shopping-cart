f123980ace68949b67826a5447be6b7cc57c1179
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-04 18:49:54 +0200

    Add Hero section to the Homepage component

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,9 +1,11 @@
 import Header from '../../sections/header/Header';
+import Hero from './components/Hero';
 
 const Homepage = ({ cartCount }) => {
   return (
     <main className="homepage">
       <Header cartCount={cartCount} />
+      <Hero />
     </main>
   );
 };
