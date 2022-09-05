3b7dc5aaedf6f8a81624f3f3c07b2b5482555e7c
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-05 14:13:22 +0200

    Add Info section into the Homepage and update Homepage tests

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,12 +1,14 @@
 import Footer from '../../sections/footer/Footer';
 import Header from '../../sections/header/Header';
 import Hero from './components/Hero';
+import Info from './components/Info';
 
 const Homepage = ({ cartCount }) => {
   return (
     <main className="homepage">
       <Header cartCount={cartCount} />
       <Hero />
+      <Info />
       <Footer />
     </main>
   );
--- src/pages/homepage/__tests__/Homepage.test.js
+++ src/pages/homepage/__tests__/Homepage.test.js
@@ -6,8 +6,16 @@ jest.mock('../components/Hero', () => () => (
   <section data-testid="hero"></section>
 ));
 
+jest.mock('../components/Info', () => () => (
+  <section data-testid="info"></section>
+));
+
 jest.mock('../../../sections/header/Header', () => () => (
   <header data-testid="header"></header>
+));
+
+jest.mock('../../../sections/footer/Footer', () => () => (
+  <footer data-testid="footer"></footer>
 ));
 
 describe('Homepage', () => {
@@ -25,5 +33,15 @@ describe('Homepage', () => {
     render(<Homepage />);
     const hero = screen.getByTestId('hero');
     expect(hero).toBeInTheDocument();
+  });
+  it('renders info component inside', () => {
+    render(<Homepage />);
+    const info = screen.getByTestId('info');
+    expect(info).toBeInTheDocument();
+  });
+  it('renders footer component inside', () => {
+    render(<Homepage />);
+    const footer = screen.getByTestId('footer');
+    expect(footer).toBeInTheDocument();
   });
 });
