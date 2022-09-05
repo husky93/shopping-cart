ab868cd80d6cfdf92e7ead2ad986035b96b98875
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-05 21:37:03 +0200

    Create Products section component

---
+++ src/pages/shop/components/Products.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from './Card';
+
+const Products = ({ products }) => {
+  return (
+    <section aria-label="Products" className="products">
+      {products.map((prod) => (
+        <Card
+          id={prod.id}
+          key={'product-card-' + prod.id}
+          text={prod.text}
+          description={prod.description}
+          price={prod.price}
+          img={prod.img}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default Products;
