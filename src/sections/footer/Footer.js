3f019d539c956f113caa1d49247dfeaa656c15af
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-05 19:35:44 +0200

    Style footer section

--- src/sections/footer/Footer.js
+++ src/sections/footer/Footer.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import Text from '../../components/Text';
-import { Link } from 'react-router-dom';
 import FooterNav from './components/FooterNav';
 import '../../assets/styles/sections/Footer.css';
 
 const Footer = () => {
   const githubLink = (
-    <Link to="https://github.com/husky93" className="author" key="author">
+    <a href="https://github.com/husky93" className="author" key="author">
       husky93
-    </Link>
+    </a>
   );
   return (
     <footer className="footer">
       <div className="content footer__content">
-        <Text text={['© ', githubLink, ' 2022']} tag="span" />
+        <Text text={['Forged by ', githubLink, ' 2022']} tag="span" />
         <FooterNav />
       </div>
     </footer>
