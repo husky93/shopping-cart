db514f6b232d2eb82e292f30dd3cf9bcf6b20b3b
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-06 12:51:11 +0200

    Test if provided props to the Card component are being used correctly

--- src/pages/shop/__tests__/Card.test.js
+++ src/pages/shop/__tests__/Card.test.js
@@ -5,6 +5,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 
 const id = '0';
+const text = 'Test text';
+const description = 'Test description';
+const price = '23$';
+const image = 'test.jpg';
 
 describe('Card', () => {
   it('renders products section', () => {
@@ -19,11 +23,25 @@ describe('Card', () => {
   it('has an image', () => {
     render(
       <Router>
-        <Card />
+        <Card img={image} />
       </Router>
     );
     const img = screen.getByRole('img');
     expect(img).toBeInTheDocument();
+  });
+  it('renders provided text, description and price', () => {
+    render(
+      <Router>
+        <Card text={text} description={description} price={price} />
+      </Router>
+    );
+    const textNode = screen.getByText('Test text');
+    const descNode = screen.getByText('Test description');
+    const priceNode = screen.getByText('23$');
+
+    expect(textNode).toBeInTheDocument();
+    expect(descNode).toBeInTheDocument();
+    expect(priceNode).toBeInTheDocument();
   });
   it('card redirects user to correct page when clicked', () => {
     render(
