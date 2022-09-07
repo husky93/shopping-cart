946f1cc446def9225b6a5a29bf23939e87a0d1d2
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-07 18:39:54 +0200

    Update build

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/Homepage';
 import Cart from './pages/cart/Cart';
 import Shop from './pages/shop/Shop';
