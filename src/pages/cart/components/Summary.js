1909c1055759a5a1056b88bf37f865e75ff8bac2
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-07 13:16:36 +0200

    Calculate total price

--- src/pages/cart/components/Summary.js
+++ src/pages/cart/components/Summary.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Text from '../../../components/Text';
+import Button from '../../../components/Button';
 
-const Summary = () => {};
+const Summary = ({ cart }) => {
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    const sum = cart.reduce(
+      (previousValue, cartItem) =>
+        previousValue + parseInt(cartItem.price, 10) * cartItem.count,
+      0
+    );
+    setTotalPrice(sum);
+  }, [cart]);
+
+  return <section aria-label="Cart summary"></section>;
+};
 
 export default Summary;
