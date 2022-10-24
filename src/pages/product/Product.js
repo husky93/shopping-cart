ca8bbcc3a32c97d68bb1d803eff465b8968becf9
Maciej Sroka <sroka.maciej93@gmail.com>
2022-10-24 10:26:45 +0200

    Bugfix - now Product page doesnt show error while searching for product data

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -8,16 +8,19 @@ const Footer = React.lazy(() => import('../../sections/footer/Footer'));
 const Text = React.lazy(() => import('../../components/Text'));
 const Button = React.lazy(() => import('../../components/Button'));
 const Counter = React.lazy(() => import('../../components/Counter'));
+const Loading = React.lazy(() => import('../loading/Loading'));
 
 const Product = ({ products, addToCart, cartCount }) => {
   const [product, setProduct] = useState(null);
   const [count, setCount] = useState(1);
+  const [loading, setLoading] = useState(true);
   const { containerRef, isVisible } = useObserver();
   const params = useParams();
 
   useEffect(() => {
     const currentProduct = products.find((prod) => prod.id === params.id);
     setProduct(currentProduct);
+    setLoading(false);
   }, [params.id, products, product]);
 
   const handleIncrement = () => {
@@ -32,29 +35,13 @@ const Product = ({ products, addToCart, cartCount }) => {
     }
   };
 
-  if (!product) {
-    return (
-      <main className="product" ref={containerRef}>
-        <Header cartCount={cartCount} />
-        <div className="content product__content">
-          <Text
-            className="error__heading"
-            tag="h2"
-            text="ERROR 404 PRODUCT NOT FOUND"
-          />
-        </div>
-        <Footer />
-      </main>
-    );
-  } else {
-    return (
-      <main className="product" ref={containerRef}>
-        <Header cartCount={cartCount} />
-        <div
-          className={`content product__content enter-transition ${
-            isVisible ? 'in-viewport' : ''
-          }`}
-        >
+  const renderContent = () => {
+    if (loading) {
+      return <Loading />;
+    }
+    if (product) {
+      return (
+        <>
           <div className="product__container product__gallery">
             <img
               src={product.img}
@@ -106,11 +93,32 @@ const Product = ({ products, addToCart, cartCount }) => {
               }
             />
           </div>
-        </div>
-        <Footer />
-      </main>
+        </>
+      );
+    }
+
+    return (
+      <Text
+        className="error__heading"
+        tag="h2"
+        text="ERROR 404 PRODUCT NOT FOUND"
+      />
     );
-  }
+  };
+
+  return (
+    <main className="product" ref={containerRef}>
+      <Header cartCount={cartCount} />
+      <div
+        className={`content product__content enter-transition ${
+          isVisible ? 'in-viewport' : ''
+        }`}
+      >
+        {renderContent()}
+      </div>
+      <Footer />
+    </main>
+  );
 };
 
 export default Product;
